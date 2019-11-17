b31e31c8e4b0ed661733221638005aa2c0a9d737
Igor Fedorov <fedorovigor1155@gmail.com>
2019-11-17 03:21:37 +0300

    uddate

--- Glassess.py
+++ Glassess.py
@@ -28,6 +28,67 @@ import cv2
 # Check available GPU devices.
 print("The following GPU devices are available: %s" % tf.test.gpu_device_name())
 
+
+class CNN(object):
+
+    def __init__(self, model_filepath):
+
+        # The file path of model
+        self.model_filepath = model_filepath
+        # Initialize the model
+        self.load_graph(model_filepath=self.model_filepath)
+
+    def load_graph(self, model_filepath):
+        '''
+        Lode trained model.
+        '''
+        print('Loading model...')
+        self.graph = tf.Graph()
+
+        with tf.io.gfile.GFile(model_filepath, 'rb') as f:
+            graph_def = tf.compat.v1.GraphDef.FromString(f.read())
+
+        print('Check out the input placeholders:')
+        nodes = [n.name + ' => ' + n.op for n in graph_def.node if n.op in ('Placeholder')]
+        for node in nodes:
+            print(node)
+
+        with self.graph.as_default():
+            # Define input tensor
+            self.input = tf.placeholder(np.float32, shape=[None, 32, 32, 3], name='input')
+            self.dropout_rate = tf.placeholder(tf.float32, shape=[], name='dropout_rate')
+            tf.import_graph_def(graph_def, {'input': self.input, 'dropout_rate': self.dropout_rate})
+
+        self.graph.finalize()
+
+        print('Model loading complete!')
+
+        # Get layer names
+        layers = [op.name for op in self.graph.get_operations()]
+        for layer in layers:
+            print(layer)
+
+        """
+        # Check out the weights of the nodes
+        weight_nodes = [n for n in graph_def.node if n.op == 'Const']
+        for n in weight_nodes:
+            print("Name of the node - %s" % n.name)
+            # print("Value - " )
+            # print(tensor_util.MakeNdarray(n.attr['value'].tensor))
+        """
+
+        # In this version, tf.InteractiveSession and tf.Session could be used interchangeably.
+        # self.sess = tf.InteractiveSession(graph = self.graph)
+        self.sess = tf.Session(graph=self.graph)
+
+    def test(self, data):
+
+        # Know your output node name
+        output_tensor = self.graph.get_tensor_by_name("import/cnn/output:0")
+        output = self.sess.run(output_tensor, feed_dict={self.input: data, self.dropout_rate: 0})
+
+        return output
+
 def display_image(image):
   cv2.imshow("img", image)
   cv2.waitKey(1)
@@ -157,12 +218,14 @@ def run_detector(detector, img):
 # To capture video from webcam.
 cap = cv2.VideoCapture(0)
 
+# cnn = CNN("C:\\Users\\Igor\\work\\prom_hack\\ProHack_Agro\\tf\\saved_model.pb")
 
 while True:
     # Read the frame
     _, img = cap.read()
     # Convert to grayscale
     start_time = time.time()
+    cnn.test(cap.read()[1])
     run_detector(detector, cap.read()[1])
     end_time = time.time()
     print("Inference time:")
